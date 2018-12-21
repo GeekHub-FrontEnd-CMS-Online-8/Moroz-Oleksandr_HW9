ddb61f8a542c6623c6e85e8d8b5ae3ab12bd3234
chwert <ostapenkomoroz1@gmail.com>
2018-12-21 02:30:49 +0200

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	
+	//hidden menu
+	$( '.btn-menu, .hidden-menu ul a' ).click( function() {
+		if ( $( '.hidden-menu' ).is( ':hidden' ) ) {
+			$( '.hidden-menu' ).show();
+		} else {
+			$( '.hidden-menu' ).hide();
+		}
+	});
+});
